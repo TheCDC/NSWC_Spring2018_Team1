358a62de3fbb4c1703b4d07271a1282c45eecde7
chrisfernandes102 <cjf.fernandes7@gmail.com>
2018-02-27 14:45:36 -0800

    Changed the program ao that it could analyze tiffs

--- Snippets/ocr.py
+++ Snippets/ocr.py
@@ -60,7 +60,7 @@ while rval:
 
     # write the grayscale image to disk as a temporary file so we can
     # apply OCR to it
-    filename = "{}.png".format(os.getpid())
+    filename = "{}.tiff".format(os.getpid())
     cv2.imwrite(filename, gray)
 
     # load the image as a PIL/Pillow image, apply OCR, and then delete
