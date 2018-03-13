470e0a24e1ae5b707fa0bb1bb9b60cfa7ddd7b68
Christopher Chen <christopher.chen1995@gmail.com>
2018-03-13 14:42:13 -0700

    Basic file uploading webapp

--- flask_webapp/__init__.py
+++ flask_webapp/__init__.py
@@ -1,9 +1,12 @@
 import flask
-import views
-
+from . import views
+import random
 app = flask.Flask(__name__)
+# necessary for CSRF
+app.config['SECRET_KEY'] = str(int(random.random() * 100000000000))
 
 app.add_url_rule('/', view_func=views.IndexView.as_view('index'))
+app.add_url_rule('/upload', view_func=views.UploadView.as_view('upload'))
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
--- flask_webapp/views.py
+++ flask_webapp/views.py
@@ -1,11 +1,34 @@
+"""Class based views for the webapp."""
 import flask
-
-from flask.views import View, MethodView
+from flask.views import MethodView
+from . import forms
 
 
 class IndexView(MethodView):
+    """The home  page."""
+
     def get_template_name(self):
         return 'index.html'
 
+    def get_context(self, request, **kwargs):
+        """Process data given in the request."""
+        form = forms.UploadForm()
+        context = dict(
+            form=form, success=request.args.get('successful_upload'))
+        context.update(**kwargs)
+        return context
+
     def get(self, **kwargs):
-        return flask.render_template(self.get_template_name())
+        """Handle get requests."""
+        return flask.render_template(
+            self.get_template_name(),
+            context=self.get_context(flask.request, **kwargs))
+
+
+class UploadView(MethodView):
+    def post(self, **kwargs):
+        form = forms.UploadForm()
+        if form.validate_on_submit():
+            return flask.redirect(
+                flask.url_for('index', successful_upload=True))
+        return flask.redirect(flask.url_for('index', successful_upload=False))
