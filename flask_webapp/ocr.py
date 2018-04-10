755e999f733054868991bbdf0224758498ccabc0
Christopher Chen <christopher.chen1995@gmail.com>
2018-04-10 14:16:03 -0700

    Implement filtering serial number from string

--- flask_webapp/ocr.py
+++ flask_webapp/ocr.py
@@ -1,6 +1,9 @@
 from PIL import Image
 import pytesseract
 import cv2
+import re
+
+serial_pattern = re.compile(r'[A-Z]{3}[0-9]{2}[A-Z]{1}[0-9]{3}-[0-9]{3}')
 
 
 def ocr_file(path, blockSize=115, C=50):
@@ -14,3 +17,10 @@ def ocr_file(path, blockSize=115, C=50):
                                      cv2.THRESH_BINARY, blockSize, C)
     return pytesseract.image_to_string(
         Image.fromarray(cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)))
+
+
+def filter_serial(s):
+    found = serial_pattern.search(s)
+    if found is not None:
+        return found.match
+    return found
